1056a147323504fb9243696dbfb7ee96c9b03510
Jody Jones <jodybjones@yahoo.com>
2019-11-06 23:08:21 -0500

    added a few quick questions. Need to make sure that they are referenced after button is clicked on initial page.

--- assets/questions.js
+++ assets/questions.js
@@ -1,1 +1,30 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
 
+    {
+      title: "Which is used mainly for debugging purposes?",
+      choices: ["cdn", "console.log", "bootstrap", "array"] ,
+      answer: "console.log" 
+    },
+    
+    {
+       title: "How do you create new folder from your terminal?",
+       choices: ["pwd", "rm", "cd", "mkdir"] ,
+       answer: "mkdir"
+
+    },
+
+    {
+        title: "How can you access every part of an array?",
+        choices: [".everything", ".allaccess", ".length", ".all"],
+        answer: ".length"
+    }]
